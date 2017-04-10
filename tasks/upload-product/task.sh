84e9706b7a8b3527a90481973ebf000192956206
matthewcosgrove <mcosgrove@pivotal.io>
2017-04-10 14:17:35 +0100

    Fix missing binary issue with more foolproof approach

--- tasks/upload-product/task.sh
+++ tasks/upload-product/task.sh
@@ -4,7 +4,9 @@ if [[ ! -z "$NO_PROXY" ]]; then
   echo "$OM_IP $OPS_MGR_HOST" >> /etc/hosts
 fi
 
-PIVNET_CLI=`find ./pivnet-cli -name "*linux-amd64*"`
+PIVNET_CLI=`pivnet-cli/pivnet-cli`
+wget -O $PIVNET_CLI -nc  https://github.com/pivotal-cf/pivnet-cli/releases/download/v0.0.49/pivnet-linux-amd64-0.0.49
+
 chmod +x $PIVNET_CLI
 
 chmod +x om-cli/om-linux
